41cbf4d29e2595a90aae198353933f149f0a4a51
lizlgit <codelzl@foxmail.com>
2018-09-04 10:13:28 +0800

    加密加盐

--- shapasswd.py
+++ shapasswd.py
@@ -3,7 +3,7 @@
 import random
 from hashlib import sha256
 
-def hash_password(password, salt=''):
+def hash_password(password, salt='lizhenlong'):
     """简单的加密密码的HASH算法，SHA256加密，固定加salt，三轮HASH"""
     result = str(password) + salt
     for i in range(3):
